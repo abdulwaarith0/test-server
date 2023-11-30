18a0a81b3d6cfa2326fbe2171d91fc1c23aa2e45
Abdulwaarith Zakariyya <abdulwaarithz@gmail.com>
2023-11-30 00:16:42 -0800

    made some changes

--- dist/routes/index.js
+++ dist/routes/index.js
@@ -6,6 +6,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const sectors_1 = __importDefault(require("./sectors"));
 const router = (0, express_1.Router)();
-router.get("/", (_, res) => res.status(200).json("pong!"));
+// router.get("/",  (_, res: Response) => res.status(200).json("pong!"))
 router.use(sectors_1.default);
 exports.default = router;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,9 @@
-import { Router, Response } from "express";
+import { Router } from "express";
 import sectorRoutes from "./sectors";
 
 const router = Router();
 
-router.get("/",  (_, res: Response) => res.status(200).json("pong!"))
+// router.get("/",  (_, res: Response) => res.status(200).json("pong!"))
 router.use(sectorRoutes);
 
 export default router;
